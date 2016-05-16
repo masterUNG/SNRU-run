c8e8493fa48d990b58fd404354d0a79d8b88ee0e
masterUNG <phrombutr@gmail.com>
2016-05-16 15:41:00 +0700

    Last

---
+++ app/src/main/java/appewtc/masterung/snrurun/MyAlert.java
@@ -0,0 +1,17 @@
+package appewtc.masterung.snrurun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 5/16/16 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/snrurun/SignUp.java
+++ app/src/main/java/appewtc/masterung/snrurun/SignUp.java
@@ -1,13 +1,54 @@
 package appewtc.masterung.snrurun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUp extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton, choice5RadioButton;
+    private String nameString, userString, passwordString, avataString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+
     }
-}
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value From EditText
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+
+    }
+
+}   // Main Class
