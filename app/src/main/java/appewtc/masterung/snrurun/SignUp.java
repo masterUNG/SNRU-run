e26f65b3ad7b976920a5696e30508c54003a9a29
masterUNG <phrombutr@gmail.com>
2016-05-16 12:02:17 +0700

    SignUp Finish

---
+++ app/src/main/java/appewtc/masterung/snrurun/SignUp.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.snrurun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
