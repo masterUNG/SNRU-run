5d3982ff114a5a482d616237118d6d53e0bd3fb6
masterUNG <phrombutr@gmail.com>
2016-05-16 13:33:15 +0700

    Intent to SignUp finish

--- app/src/main/java/appewtc/masterung/snrurun/MainActivity.java
+++ app/src/main/java/appewtc/masterung/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package appewtc.masterung.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
     }
-}
+
+}   // Main Class นี่คือ คลาสหลัก
