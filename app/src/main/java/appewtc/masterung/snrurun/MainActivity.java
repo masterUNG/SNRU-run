79501d0609f3e92d45bf981ab32be358632d21c3
masterUNG <phrombutr@gmail.com>
2016-05-17 16:20:39 +0700

    Delete SQLite

--- app/src/main/java/appewtc/masterung/snrurun/MainActivity.java
+++ app/src/main/java/appewtc/masterung/snrurun/MainActivity.java
@@ -1,6 +1,7 @@
 package appewtc.masterung.snrurun;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(MainActivity.this);
 
+        //Test Add user
+        //myManage.addUser("มาสเตอร์ อึ่ง", "master", "12345", "2");
+
+        //Delete All SQLite
+        deleteAllSQLite();
+
     }   // Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUp.class));
--- app/src/main/java/appewtc/masterung/snrurun/MyManage.java
+++ app/src/main/java/appewtc/masterung/snrurun/MyManage.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.snrurun;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,11 +13,33 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_avata = "Avata";
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }   // Constructor
+
+    public long addUser(String strName,
+                        String strUser,
+                        String strPassword,
+                        String strAvata) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_name, strName);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+        contentValues.put(column_avata, strAvata);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
 
 }   // Main Class
