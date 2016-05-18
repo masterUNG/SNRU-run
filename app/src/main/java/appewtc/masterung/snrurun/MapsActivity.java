d269716f38c8a7fe84cdd71cf51022bab4ab9581
masterUNG <phrombutr@gmail.com>
2016-05-18 16:00:45 +0700

    Get GPS Finish

--- app/src/main/java/appewtc/masterung/snrurun/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/snrurun/MapsActivity.java
@@ -5,10 +5,10 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -44,13 +44,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }   // Main Method
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+       // locationManager.removeUpdates((android.location.LocationListener) locationListener);
+        myLatADouble = snruLatADouble;
+        myLngADouble = snruLngADouble;
+
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER, "ไม่ได้ต่อเน็ต");
+        if (networkLocation != null) {
+            myLatADouble = networkLocation.getLatitude();
+            myLngADouble = networkLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER, "ไม่มี GPS");
+        if (gpsLocation != null) {
+            myLatADouble = gpsLocation.getLatitude();
+            myLngADouble = gpsLocation.getLongitude();
+        }
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
+    }
+
     public Location myFindLocation(String strProvider, String strError) {
 
         Location location = null;
 
         if (locationManager.isProviderEnabled(strProvider)) {
 
-            locationManager.requestLocationUpdates(strProvider, 1000, 10, (android.location.LocationListener) locationListener);
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
             location = locationManager.getLastKnownLocation(strProvider);
 
         } else {
@@ -62,15 +93,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     //Create Inner Class
-    public LocationListener locationListener = new LocationListener() {
+   public android.location.LocationListener locationListener = new android.location.LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-
             myLatADouble = location.getLatitude();
             myLngADouble = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
 
         }
-    }; // Location
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
 
 
@@ -82,6 +126,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng snruLatLng = new LatLng(snruLatADouble, snruLngADouble);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(snruLatLng, 15));
 
+        //My Loop
+        myLoop();
+
     }   // onMapReady
+
+    private void myLoop() {
+
+        Log.d("18May16", "myLat = " + myLatADouble);
+        Log.d("18May16", "myLng = " + myLngADouble);
+
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                myLoop();
+            }
+        }, 3000);
+
+    }   // myLoop
 
 }   // Main Class
