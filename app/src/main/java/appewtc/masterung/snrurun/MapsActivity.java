488cd528e67ad130e8fc8eb78648da90f0368edc
masterUNG <phrombutr@gmail.com>
2016-05-19 13:18:07 +0700

    Add Method for distance

--- app/src/main/java/appewtc/masterung/snrurun/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/snrurun/MapsActivity.java
@@ -66,6 +66,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }   // Main Method
 
+    //นี่คือ เมทอด ที่หาระยะ ระหว่างจุด
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515 * 1.609344;
+
+
+        return (dist);
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
+
     //Inner Class
     public class SynLocation extends AsyncTask<Void, Void, String> {
 
