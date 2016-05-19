e2d89b8400def9fe98bdd4a151b498a4c053d4d4
masterUNG <phrombutr@gmail.com>
2016-05-19 14:49:00 +0700

    Exercise

--- app/src/main/java/appewtc/masterung/snrurun/Exercise.java
+++ app/src/main/java/appewtc/masterung/snrurun/Exercise.java
@@ -1,13 +1,96 @@
 package appewtc.masterung.snrurun;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Exercise extends AppCompatActivity {
+
+    //Explicit
+    private TextView textView;
+    private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        bindWidget();
+
+    }   // Main Method
+
+    public class SynQuestion extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/snru/get_question.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+           // return null;
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                String[] questionStrings = new String[jsonArray.length()];
+                String[] choice1Strings = new String[jsonArray.length()];
+                String[] choice2Strings = new String[jsonArray.length()];
+                String[] choice3Strings = new String[jsonArray.length()];
+                String[] choice4Strings = new String[jsonArray.length()];
+                String[] answerStrings = new String[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                }   // for
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }   // onPost
+
+    }   // SynQuestion Class
+
+
+    private void bindWidget() {
+
+        textView = (TextView) findViewById(R.id.textView6);
+        radioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton6);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton7);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton8);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton9);
+
     }
-}
+
+}   // Main Class
